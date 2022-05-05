1b749a3ec7d3d352d5a0917cec9ae203b0cd9bcf
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2022-05-05 15:19:39 -0400

    fix(sql): fix some wrong values in sql file

--- Framework.sql
+++ Framework.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE IF NOT EXISTS `Framework`;
-USE `Framework`;
+CREATE DATABASE IF NOT EXISTS `framework`;
+USE `framework`;
 
-ALTER DATABASE `Framework`
+ALTER DATABASE `framework`
 	DEFAULT CHARACTER SET UTF8MB4;
 	
-ALTER DATABASE `Framework`
+ALTER DATABASE `framework`
 	DEFAULT COLLATE UTF8MB4_UNICODE_CI;
 
 CREATE TABLE `users` (
@@ -23,14 +23,13 @@ CREATE TABLE `users` (
   	`dateofbirth` varchar(10) DEFAULT NULL,
   	`sex` varchar(1) DEFAULT NULL,
   	`height` int(11) DEFAULT NULL,
-	--`charinfo` TEXT DEFAULT NULL,
 	`metadata` TEXT NULL DEFAULT NULL,
 	`skin` LONGTEXT DEFAULT NULL,
 	`is_dead` tinyint(1) DEFAULT 0,
 	`last_updated` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
 	PRIMARY KEY (`citizenid`),
 	KEY `identifier` (`identifier`),
-	KEY `last_updated` (`last_updated`),
+	KEY `last_updated` (`last_updated`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE `jobs` (
@@ -73,9 +72,7 @@ CREATE TABLE `items` (
 	`name` VARCHAR(50) NOT NULL,
 	`label` VARCHAR(50) NOT NULL,
 	`weight` INT NOT NULL DEFAULT 1,
-	--`rare` TINYINT NOT NULL DEFAULT 0,
 	`can_remove` TINYINT NOT NULL DEFAULT 1,
-
 	PRIMARY KEY (`name`)
 ) ENGINE=InnoDB;
 
@@ -96,5 +93,5 @@ CREATE TABLE IF NOT EXISTS `bans` (
 	KEY `ip` (`ip`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
 
-INSERT INTO `jobs` VALUES ('unemployed','Unemployed');
+INSERT INTO `jobs` VALUES ('unemployed', 'Unemployed', 0);
 INSERT INTO `job_grades` VALUES (1, 'unemployed', 0, 'unemployed', 'Unemployed', 0, 200, '{}', '{}');
